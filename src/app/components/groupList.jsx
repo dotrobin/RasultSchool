7e9ef08eba4016d9e263c3020171fa4607794e28
Kirill <dotslash.dot@yandex.ru>
2022-10-10 14:04:07 +0300

    add GroupList functional for processing arrays and objectsЭ

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import _ from "lodash";
 
 const GroupList = ({ items, valueProperty, contentProperty, selectedItem, onItemSelect }) => {
 	return (
 		<ul className="list-group">
-			{Object.keys(items).map(item => (
-				<li
-					role="button"
-					key={items[item][valueProperty]}
-					className={"list-group-item" + (items[item] === selectedItem ? " active" : "")}
-					onClick={() => onItemSelect(items[item])}
-				>
-					{items[item][contentProperty]}
-				</li>
-			))}
+			{items instanceof Object
+				? Object.keys(items).map(item => (
+					<li
+						role="button"
+						key={items[item][valueProperty]}
+						className={"list-group-item" + (_.isEqual(items[item], selectedItem) ? " active" : "")}
+						onClick={() => onItemSelect(items[item])}
+					>
+						{items[item][contentProperty]}
+					</li>
+				))
+				: items.map(
+					item => (
+						<li
+							role="button"
+							key={item[valueProperty]}
+							className={"list-group-item" + (_.isEqual(item, selectedItem) ? " active" : "")}
+							onClick={() => onItemSelect(item)}
+						>
+							{item[contentProperty]}
+						</li>
+					)
+				)
+			}
 		</ul>
 	);
 };
