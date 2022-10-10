380cc48809933f3d3e58456da16d5c025f6b955b
Kirill <arist84@mail.ru>
2022-10-10 04:10:29 +0300

    ups... back to origin api

--- src/app/api/fake.api/professions.api.js
+++ src/app/api/fake.api/professions.api.js
@@ -19,7 +19,7 @@ export const professions = [
 const fetchAll = () =>
 	new Promise((resolve) => {
 		window.setTimeout(function () {
-			resolve(professionsObject);
+			resolve(professions);
 		}, 500);
 	});
 
