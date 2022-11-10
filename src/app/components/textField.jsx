de207a35ee1caf3a453b2aa2d1cdc0bae4070999
Kirill <arist84@mail.ru>
2022-11-11 00:43:16 +0300

    error with error meassage fixed

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 const TextField = ({ label, type, name, value, onChange, error }) => {
 	const [showPassword, setShowPassword] = useState(false);
 	const getInputClasses = () => {
-		console.log(error);
 		return "form-control" + (error ? " is-invalid" : "");
 	};
 
