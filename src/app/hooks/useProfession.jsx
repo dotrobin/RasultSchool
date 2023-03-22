ec124fe33dcbfecf41fc8a90512dc087df1a675d
Kirill <arist84@mail.ru>
2023-03-23 00:15:56 +0300

    ln14 done

---
+++ src/app/hooks/useProfession.jsx
@@ -0,0 +1,61 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { toast } from "react-toastify";
+
+import professionService from "../services/professiona.service";
+
+const ProfessionContext = React.createContext();
+
+export const useProfessions = () => {
+  return useContext(ProfessionContext);
+};
+
+export const ProfessionProvider = ({ children }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [professions, setProfessions] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getProfessionsList();
+    setError(null);
+  }, []);
+
+  useEffect(() => {
+    if (error !== null) {
+      toast(error);
+    };
+  }, [error]);
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  };
+
+  async function getProfessionsList() {
+    try {
+      const { content } = await professionService.get();
+      setProfessions(content);
+      setLoading(false);
+    } catch (error) {
+      errorCatcher(error);
+    }
+  };
+
+  function getProfession(id) {
+    return professions.find((p) => p._id === id);
+  };
+
+  return <ProfessionContext.Provider
+    value={{
+      isLoading,
+      professions,
+      getProfession
+    }}
+  >
+    {children}
+  </ProfessionContext.Provider>;
+};
+
+ProfessionProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+};
---
+++ src/app/hooks/useUsers.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useContext, useEffect } from "react";
+import PropTypes from "prop-types";
+import { toast } from "react-toastify";
+
+import userService from "../services/user.service";
+
+const UserContext = React.createContext();
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
+
+const UserProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getUsers();
+    setError(null);
+  }, []);
+
+  useEffect(() => {
+    if (error !== null) {
+      toast(error);
+    };
+  }, [error]);
+
+  async function getUsers() {
+    try {
+      const { content } = await userService.get();
+      setUsers(content);
+      setLoading(false);
+    } catch (error) {
+      errorCatcher(error);
+    }
+  };
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        users
+      }}
+    >
+      {!isLoading ? children : "Loading..."}
+    </UserContext.Provider>
+  );
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+};
+
+export default UserProvider;
